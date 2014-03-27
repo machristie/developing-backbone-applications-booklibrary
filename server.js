a3b0366488882aafc1597552175fe01e6102094e
Marc Christie <mchristie@walkerinfo.com>
2014-03-27 10:30:43 -0400

    adding other RESTful methods

--- server.js
+++ server.js
@@ -59,3 +59,67 @@ app.get( '/api/books', function( request, response ) {
         }
     });
 });
+
+//Insert a new book
+app.post( '/api/books', function( request, response ) {
+    var book = new BookModel({
+        title: request.body.title,
+        author: request.body.author,
+        releaseDate: request.body.releaseDate
+    });
+    book.save( function( err ) {
+        if( !err ) {
+            console.log( 'created' );
+        } else {
+            console.log( err );
+        }
+        return response.send( book );
+    });
+});
+
+
+// Get a single book by id
+app.get( '/api/books/:id', function( request, response ) {
+    return BookModel.findById( request.params.id, function( err, book ) {
+        if( !err ){
+            return response.send(book);
+        } else {
+            return console.log( err );
+        }
+    });
+});
+
+// Update a book
+app.put( '/api/books/:id', function( request, response ) {
+
+    return BookModel.findById( request.params.id, function( err, book ) {
+        book.title = request.body.title;
+        book.author = request.body.author;
+        book.releaseDate = request.body.releaseDate;
+
+        return book.save( function( err ) {
+            if( !err ){
+                return response.send( book );
+            } else {
+                return console.log( err );
+            }
+        });
+    });
+});
+
+
+// Delete a book
+app.delete( '/api/books/:id', function( request, response ) {
+
+    console.log( 'Deleting book with id: ' + request.params.id );
+    return BookModel.findById( request.params.id, function( err, book ) {
+        return book.remove( function( err ) {
+            if( !err ){
+                console.log( 'Book removed' );
+                return response.send( '' );
+            } else {
+                return console.log( err );
+            }
+        });
+    });
+});
